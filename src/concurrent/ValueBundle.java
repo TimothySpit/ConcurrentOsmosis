60e265294de065950ab64b96f859cbd70ff55e16
Magnus Halbe <s9mahalb@stud.uni-saarland.de>
2015-08-13 12:10:24 +0200

    Defined ValueBundle

--- src/concurrent/ValueBundle.java
+++ src/concurrent/ValueBundle.java
@@ -3,12 +3,28 @@ package concurrent;
 import gnu.trove.list.array.TDoubleArrayList;
 
 public class ValueBundle {
-
-	public ValueBundle() {
-		TDoubleArrayList values;
-		int convergent = 0;
-		int currentMaxSteps;
-
+	TDoubleArrayList values;
+	int convergents = 0;
+	int currentMaxSteps;
+	
+	public ValueBundle(TDoubleArrayList values, int convergents, int currentMaxSteps) {
+		this.values = values;
+		this.currentMaxSteps = currentMaxSteps;
+		this.convergents = convergents;
 	}
-
+	
+	public TDoubleArrayList getValues()
+	{
+		return values;
+	}
+	
+	public int getConvergents()
+	{
+		return convergents;
+	}
+	
+	public int getCurrentMaxSteps()
+	{
+		return currentMaxSteps;
+	}
 }
