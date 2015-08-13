a9883fd995a5abe2caa50251cbddeec32a9744a4
Magnus Halbe <s9mahalb@stud.uni-saarland.de>
2015-08-13 12:03:58 +0200

    ValueBundle

---
+++ src/concurrent/ValueBundle.java
@@ -0,0 +1,14 @@
+package concurrent;
+
+import gnu.trove.list.array.TDoubleArrayList;
+
+public class ValueBundle {
+
+	public ValueBundle() {
+		TDoubleArrayList values;
+		int convergent = 0;
+		int currentMaxSteps;
+
+	}
+
+}
