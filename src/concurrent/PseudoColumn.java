2a9de4f22db9163a1dac31ef4bb57c0cd4c64a38
Timo Speith <s9tospei@stud.uni-saarland.de>
2015-08-15 12:08:43 +0200

    PseuCo

--- src/concurrent/PseudoColumn.java
+++ src/concurrent/PseudoColumn.java
@@ -116,7 +116,7 @@ public class PseudoColumn
                     int convergents = hConvergents + vConvergents;
                     if(convergents == 0)
                     {increaseSteps();}
-                    else if(convergents < ((columnCount*2)-1))
+                    else if(convergents < ((columnCount*2)-1)) //hConv is Columns -1
                     {
                         reduceSteps();
                         System.out.println("Convergent: H: " + hConvergents + ", V: " + vConvergents);
