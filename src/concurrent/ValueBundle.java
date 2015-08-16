d09a2c407e59ce99aba12067d68d194e4d231e88
Timo Speith <s9tospei@stud.uni-saarland.de>
2015-08-16 12:19:02 +0200

    The mighty Magnus!

--- src/concurrent/ValueBundle.java
+++ src/concurrent/ValueBundle.java
@@ -6,12 +6,12 @@ import gnu.trove.list.array.TDoubleArrayList;
  * Bundle of values which is passed via Exchangers between different columns
  *
  * @author Timo Speith and Magnus Halbe
- * @version 1.4
+ * @version 2.0
  */
 public class ValueBundle
 {
-        private TDoubleArrayList 
-	private TDoubleArrayList values; // List of all values
+        private TDoubleArrayList values;
+	private TDoubleArrayList pass; // List of values to pass
 	private int hConvergents = 0; // Number of horizontal convergent columns
         private int vConvergents = 0; // Number of vertical convergent columns
 	private int currentSteps; // Steps to use
@@ -19,14 +19,17 @@ public class ValueBundle
         /**
          * Creates a new instance of ValueBundle
          *
-         * @param values the values which should be passed
+         * @param values the values of all nodes
+         * @param pass the values which should be passed
          * @param hConvergents the number of horizontal convergent threads until now
          * @param vConvergents the number of vertical convergent threads until now
          * @param currentSteps the number of steps the successor should use
          */
-	public ValueBundle(TDoubleArrayList values, int hConvergents ,int vConvergents,int currentSteps)
+	public ValueBundle(TDoubleArrayList values, TDoubleArrayList pass, int hConvergents,
+                int vConvergents, int currentSteps)
         {
             this.values = values;
+            this.pass = pass;
             this.hConvergents = hConvergents;
             this.vConvergents = vConvergents;
             this.currentSteps = currentSteps;
@@ -39,27 +42,37 @@ public class ValueBundle
          */
         public ValueBundle(int currentSteps)
         {
-            this(null, 0, 0, currentSteps);
+            this(null, null, 0, 0, currentSteps);
         }
 
         /**
          * Creates a new instance of ValueBundle with no convergents and no steps.
          *
-         * @param values the values to be passed
+         * @param pass the values to be passed
          */
-        public ValueBundle(TDoubleArrayList values)
+        public ValueBundle(TDoubleArrayList pass)
         {
-            this(values, 0, 0, 0);
+            this(null, pass, 0, 0, 0);
         }
+        
+        /**
+         * Returns all node values
+         *
+         * @return all node values
+         */
+	public TDoubleArrayList getValues()
+	{
+            return pass;
+	}
 
         /**
          * Returns this columns node-values
          *
          * @return this columns node-values
          */
-	public TDoubleArrayList getValues()
+	public TDoubleArrayList getPass()
 	{
-            return values;
+            return pass;
 	}
 
         /**
